734736ddff0303737bb1fb98236e4e5a1d2b8fae
icune <icune@ya.ru>
2019-01-17 03:00:10 +0300

    Contracts

---
+++ Token.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.24;
+
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+ * Originally based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ *
+ * This implementation emits additional Approval events, allowing applications to reconstruct the allowance status for
+ * all accounts just by listening to said events. Note that this isn't required by the specification, and other
+ * compliant implementations may not do it.
+ *
+ */
+contract PPToken is ERC20, ERC20Detailed, ERC20Burnable {
+    uint256 public constant INITIAL_SUPPLY = 1000000000000;
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor () public ERC20Detailed("PointPay Token", "PAY", 3) {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+    function getNow() public view returns(uint256) {
+        return now;
+    }
+}
